2414a234ca23fe38da18bf5a5ca61b7baed0beac
mily-chan <emilygabriellee@gmail.com>
2020-02-24 19:44:24 -0300

    pares e ímpares

---
+++ Desafio da mamae.py
@@ -0,0 +1,8 @@
+print("==== N Ú M E R O S   P A R E S ====")
+for i in range(0, 102,  +2):
+    print(i)
+print("===================================")
+print("==== N Ú M E R O S   Í M P A R E S ====")
+for i in range(99, 0, -2):
+    print(i)
+print("=======================================")
