fba1b94bb08a95b5d5c6f28e1d31715f0a9248db
mily-chan <emilygabriellee@gmail.com>
2020-02-24 17:11:42 -0300

    Dobro, triplo e raiz quadrada

---
+++ Desafio 02.py
@@ -0,0 +1,8 @@
+import math
+
+n1 = int(input("Digite um número:  "))
+
+d1 = n1 * 2
+d2 = n1 * 3
+d3 = math.sqrt(n1)
+print("o dobro de {} é {}, o triplo de {} é {} e a rsaiz quadrada de {} é {}!".format(n1, d1, n1, d2, n1, d3))
