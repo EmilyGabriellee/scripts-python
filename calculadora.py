a1bd606e5388c59849efb3a082b0138a3fcaa2b9
mily-chan <emilygabriellee@gmail.com>
2020-03-01 15:08:28 -0300

    Melhorando a calculadora

--- calculadora.py
+++ calculadora.py
@@ -5,11 +5,17 @@ def calculadora(n1, n2, operador):
        print( n1 - n2)
     elif operador == '*':
         print(n1 * n2)
-    else :
+    else:
        print( n1 / n2)
 
-Emily = int(input("Digite um número: "))
-Cintia= input("Digite o operador (+, -, *, /): ")
-Wesley = int(input("Digite outro número: "))
 
-calculadora(Emily, Wesley, Cintia)
+n1 = int(input("Digite um número: "))
+
+while True:
+    operador= input("Digite o operador válido (+, -, *, /): ")
+    if operador == '+' or operador == '-' or operador == '*' or operador == '/':
+        break
+
+n2 = int(input("Digite outro número: "))
+
+calculadora(n1, n2, operador)
