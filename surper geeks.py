ce7a570898b6edd6382732bbe10d0dd9bd1e9381
mily-chan <emilygabriellee@gmail.com>
2020-03-12 21:39:41 -0300

    Pokedex

---
+++ surper geeks.py
@@ -0,0 +1,13 @@
+pokemons = ["pikachu", "squitle", "charizite"]
+
+print("------------------------------------")
+print(pokemons)
+print("------------------------------------")
+
+pokemon = input("Qual pokemon deseja remover: ")
+pokemons.remove(pokemon)
+print("-------------------------------------")
+
+print("-------------------------------------")
+print(pokemons)
+print("-------------------------------------")
