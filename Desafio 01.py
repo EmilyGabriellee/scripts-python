6e3d6f31ef1a9ead25623369de31aec656c12229
mily-chan <emilygabriellee@gmail.com>
2020-05-07 13:42:01 -0300

    cores

--- Desafio 01.py
+++ Desafio 01.py
@@ -1,3 +1,3 @@
 n1 = int(input("Digite um número inteiro: "))
-print("O antecessor do número \033[1;31m {} \033[mé: \033[1;31m {} \033[m! ".format(n1, n1 - 1))
+print("O antecessor do número \033[1;31;42m {} \033[mé: \033[1;31m {} \033[m! ".format(n1, n1 - 1))
 print("O sucessor do número \033[1;31m {} \033[mé: \033[1;31m {} \033[m! ".format(n1, n1 + 1))
--- Desafio 02.py
+++ Desafio 02.py
@@ -5,4 +5,6 @@ n1 = int(input("Digite um número:  "))
 d1 = n1 * 2
 d2 = n1 * 3
 d3 = math.sqrt(n1)
-print("o dobro de \033[1;33m{}\033[m é \033[1;31m{}\033[m, o triplo de \033[1;33m{}\033[m é \033[1;31m{}\033[m e a rsaiz quadrada de \033[1;33m{}\033[m é \033[1;31m{}\033[m!".format(n1, d1, n1, d2, n1, d3))
+print("o dobro de \033[1;33m{}\033[m é \033[1;31m{}\033[m, o triplo de \033[1;33m{}"
+      "\033[m é \033[1;31m{}\033[m e a rsaiz quadrada de \033[1;33m{}\033[m é \033[1;31m{}"
+      "\033[m!".format(n1, d1, n1, d2, n1, d3))
