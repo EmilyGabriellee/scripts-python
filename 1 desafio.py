454786578431ccd285ad064c49a71a2167ad5622
mily-chan <emilygabriellee@gmail.com>
2020-07-01 11:02:55 -0300

    contagem regreciva

--- 1 desafio.py
+++
@@ -1,5 +0,0 @@
-dia =input("dia")
-mes =input("mes")
-ano =input("ano")
-
-print("voce nasceu no dia ",dia,"do mes de ",mes," no ano de ",ano,"correto?") 
---
+++ desafio_46.py
@@ -0,0 +1,8 @@
+from time import sleep
+for i in range(10, -1, -1):
+    print(i)
+    sleep(0.5)
+print('BUM!')
+print('BUM!')
+print('BUM!')
+print('BUM!')
